9302182343473b99b32052a96590485312717c11
Collinsac <agucollins832@gmail.com>
2024-08-07 15:32:22 +0100

    Deleted the app.css

--- Genie_IT/src/App.jsx
+++ Genie_IT/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
@@ -15,9 +14,7 @@ function App() {
       <Service />
       <OurTeam />
       <OurCreations />
-      
       <Testimonials />
-
       <Footer />
     </>
   );
