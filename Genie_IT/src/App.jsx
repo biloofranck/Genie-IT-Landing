945e018e86d0d5621f3e337719769e20885e939c
biloofranck <biloofranckbranly@gmail.com>
2024-08-07 13:30:52 +0100

    Test2

--- Genie_IT/src/App.jsx
+++ Genie_IT/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <>
      <h1 className='bg-blue-500'>Genie_IT</h1>
+     <h1 className='bg-blue-500'>Genie_IT</h1>
+     <h1 className='bg-blue-500'>Genie_IT</h1>
     </>
   )
 }
