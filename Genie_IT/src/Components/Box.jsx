efb8cfe7929a554c3ba7f58f0a446f7acaf4227a
Collinsac <agucollins832@gmail.com>
2024-08-08 13:58:32 +0100

    Changed some styling in the testimonials section

--- Genie_IT/src/Components/Box.jsx
+++ Genie_IT/src/Components/Box.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Box = () => {
   return (
     <div>
-    <div className="bg-gray-900 p-6 rounded-[40px] m-4 w-[300px] h-[280px]">
+    <div className="bg-gray-900 p-6 rounded-[40px] w-full h-[280px]">
     <p className="text-lg italic text-[55px] mt-5 "> "</p>
     <p className="text-lg italic">Et quoniam mirari posse quosdam sonare peregrinos</p>
     <div>
