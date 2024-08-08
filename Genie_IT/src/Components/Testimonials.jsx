c2def4320b5264697fa53849afd943be0f593e20
Hermann-Buns <hermannbunang95@gmail.com>
2024-08-08 13:44:27 +0100

    Testimonials

--- Genie_IT/src/Components/Testimonials.jsx
+++ Genie_IT/src/Components/Testimonials.jsx
@@ -3,16 +3,17 @@ import Box from './Box'
 // ======================================================
 // === importing image assets
 // ======================================================
-
+import bonom from "../asset/8.png"
 
 const Testimonials = () => {
   return (
     <div className="bg-gray-800 text-white rounded-[60px]  m-20 h-[450px]  ">
-    <div className='text-right'>
+      
+    <div className='text-right '> 
       <h2 className="text-[80px] font-bold  mr-14 mt-1">Avis des experts</h2>
       <p className='mt-8 ml-16 text-gray-400 italic mb-14 mr-[100px] '>We will take care of everything else</p>
     </div>
-    <div className=" grid grid-cols-3 mt-25  ">
+    <div className=" grid grid-cols-3 mt-25 ">
      <Box/>
      <Box/>
      <Box/>
