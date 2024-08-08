eb6c12224a3d740ab592b02d3e7e0b5dc0c7b1af
Collinsac <agucollins832@gmail.com>
2024-08-08 14:08:14 +0100

    removed div

--- Genie_IT/src/Components/Box.jsx
+++ Genie_IT/src/Components/Box.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Box = () => {
   return (
     <div>
-    <div className="bg-gray-900 p-6 rounded-[40px] w-full h-[280px]">
-    <p className="text-lg italic text-[55px] mt-5 "> "</p>
-    <p className="text-lg italic">Et quoniam mirari posse quosdam sonare peregrinos</p>
-    <div>
+      <div className="bg-gray-900 p-6 rounded-[40px] w-full h-[280px]">
+        <p className="text-lg italic text-[55px] mt-5 "> "</p>
+        <p className="text-lg italic">
+          Et quoniam mirari posse quosdam sonare peregrinos
+        </p>
+      </div>
     </div>
-    </div>
-   </div>
+  );
+};
 
-  )
-}
-
-export default Box
+export default Box;
