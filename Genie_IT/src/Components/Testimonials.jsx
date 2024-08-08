1fa7f139cd6a6e412e975bd82da95b3e70804aee
Hermann-Buns <hermannbunang95@gmail.com>
2024-08-08 13:51:08 +0100

    test2

--- Genie_IT/src/Components/Testimonials.jsx
+++ Genie_IT/src/Components/Testimonials.jsx
@@ -13,7 +13,7 @@ const Testimonials = () => {
       <h2 className="text-[80px] font-bold  mr-14 mt-1">Avis des experts</h2>
       <p className='mt-8 ml-16 text-gray-400 italic mb-14 mr-[100px] '>We will take care of everything else</p>
     </div>
-    <div className=" grid grid-cols-3 mt-25 ">
+    <div className=" grid grid-cols-3 mt-25  mr-10 ml-14">
      <Box/>
      <Box/>
      <Box/>
