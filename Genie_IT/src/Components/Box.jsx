f594dddbe6ca13cf06f4bcb8aa6e4a704e986696
Hermann-Buns <hermannbunang95@gmail.com>
2024-08-12 12:54:47 +0100

    testimonials2

--- Genie_IT/src/Components/Box.jsx
+++ Genie_IT/src/Components/Box.jsx
@@ -6,7 +6,7 @@ const Box = () => {
     <div>
       <div className="bg-gray-900 p-6 rounded-[40px] w-full h-[280px]">
         <p className=" italic text-[40px] mt-5 "> "</p>
-        <p className="text-lg italic">
+        <p className="text-lg italic ml-4 ">
           Et quoniam mirari posse quosdam sonare peregrinos
         </p>
         <div className="mt-4">
@@ -14,7 +14,7 @@ const Box = () => {
          <img
          src={buns}
          alt="buns image"
-         className="w-5 h-10" />
+         className="w-5 h-10 ml-4" />
          <div className="ml-8"> 
          <p className="mt-2 font-semibold">Mark Grabbs</p>
          <p className="text-sm text-gray-400">Verified Artist</p>
