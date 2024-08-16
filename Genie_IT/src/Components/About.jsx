a6e12e16088cebaea19158d55495432cf70f166d
TechHub-giftsnow <tech66hub@gmail.com>
2024-08-16 11:49:11 +0100

    About section

--- Genie_IT/src/Components/About.jsx
+++ Genie_IT/src/Components/About.jsx
@@ -5,23 +5,23 @@ import timer from "../asset/10.png";
 import CalenderIcon from "../asset/17.png";
 import StopWatchIcon from "../asset/18.png";
 
-const About = () => {
+const About1 = () => {
   return (
     <section>
-      <div className=" relative container bg-gradient-to-b from-[#099bf683] to-[#00d9e8f3] mx-auto rounded-[30px] w-[1000px] h-[500px] px-1 pt-1">
-        <div className="bg-[#225e8383] w-[100%] h-[80%] rounded-t-3xl ">
-          <div className=" absolute top-0 flex">
-            <img src={pc} className="w-[900px] h-[620px] -ml-28 -mt-4" alt="" />
-            <div className="mt-24 -ml-16 pr-12">
-              <span className="text-slate-200 font-semibold text-5xl pr-3">
+      <div className=" relative container  bg-gradient-to-b from-[#099bf683] to-[#00d9e8f3] mx-auto rounded-[30px] w-[100%] h-[100vh] ">
+        <div className="bg-[#225e8383] w-[100%] h-[80%] rounded-t-3xl  ">
+          <div className=" flex flex-col mx-auto justify-center items-center p-0  ">
+            <img src={pc} className="w-full h-[50%]  ml-8mx-auto" alt="" />
+            <div className=" ">
+              <span className="text-white font-bold text-[50px]">
                 Why Genie iT is a new begging
               </span>
-              <p className="mt-6 text-xl font-medium text-slate-700 sm:">
+              <p className="mt-6 text-xl font-medium text-slate-600 ">
                 Et quoniam mirari posse quosdam peregrinos existimo haec
                 lecturos forsitan, si contigerit, quamobrem cum oratio ad ea
                 monstranda deflexerit quae Romae gererentur
               </p>
-              <button className="bg-[#099CF6] px-8 py-2 text-slate-100 text-lg font-semibold my-2 mt-4  rounded-3xl ">
+              <button className="bg-[#099CF6] px-8 py-2 text-slate-100 text-lg font-semibold   rounded-3xl ">
                 Find my design
               </button>
             </div>
@@ -30,7 +30,7 @@ const About = () => {
         <div>
           <img
             src={phone}
-            className="w-[200px] h-[200px] rotate-45 -ml-24 -mt-16 blur-sm"
+            className="w-[150px] h-[150px] rotate-45 -ml-24 -mt-16 blur-sm"
             alt=""
           />
         </div>
@@ -39,7 +39,7 @@ const About = () => {
         </div>
       </div>
 
-      <div className="flex gap-24 justify-center mt-28  ">
+      <div className="grid grid-cols-2 md:grid-cols-4  gap-24 justify-center mt-36   ">
         <div className="flex divide-x-2 divide-violet-900">
           <div>
             <img src={CalenderIcon} className="w-[70px]" alt="" />
@@ -97,4 +97,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default About1;
